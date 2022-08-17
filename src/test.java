42654ed221f11b04a47f048500ad94839b7991ae
Phan Thanh Toan <toanpt@hybrid-technologies.vn>
2022-08-17 15:43:51 +0700

    test

--- src/test.java
+++ src/test.java
@@ -2,5 +2,11 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("testing") ;
+        System.out.println("testing") ;
+        System.out.println("testing") ;
+        System.out.println("testing") ;
+        System.out.println("testing") ;
+        System.out.println("testing") ;
+
     }
 }
