f8e733d4a213b91a6fb121ecce0b0be82de6b401
Phan Thanh Toan <toanpt@hybrid-technologies.vn>
2022-08-17 16:56:46 +0700

    t

--- src/test.java
+++ src/test.java
@@ -1,17 +1,27 @@
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("testing") ;
-        System.out.println("testing") ;
-        System.out.println("testing") ;
-        System.out.println("testing222") ;
-    int a ;
-    int bb ;int cc;
-        System.out.println("test222ing") ;
-        System.out.println("testing") ;
-        System.out.println("testing") ;
-        System.out.println("testing") ;
-        System.out.println("testing") ;
-        System.out.println("testing") ;
+        System.out.println("testing");
+        System.out.println("testing");
+        System.out.println("testing");
+        System.out.println("testing222");
+        System.out.println("test222ing");
+        System.out.println("testing");
+        System.out.println("testing");
+        System.out.println("testing");
+        System.out.println("testing");
+        System.out.println("testing");
+        System.out.println("testing");
+        System.out.println("testing222");
+        System.out.println("test222ing");
+        System.out.println("testing");
+        System.out.println("testing");
+        System.out.println("testing222");
+        System.out.println("test222ing");
+        System.out.println("testing");
+        System.out.println("testing");
+        System.out.println("testing222");
+        System.out.println("test222ing");
+        System.out.println("testing");
     }
 }
