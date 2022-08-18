c9510d0e2ea013775fdbfaba6fd8868541db3310
Phan Thanh Toan <toanpt@hybrid-technologies.vn>
2022-08-18 11:27:37 +0700

    main_s2 c

--- src/test2.java
+++ src/test2.java
@@ -1,7 +1,20 @@
 public class test2 {
     void toafn(){
+        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;
 
-
+        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;
+        System.out.println("ewew ") ;
         System.out.println("ytyty ") ;
         System.out.println("ytyty ") ;
         System.out.println("ytyty ") ;
