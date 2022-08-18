90e78e60fa0905ce7dbd11438c187a50797d062c
Phan Thanh Toan <toanpt@hybrid-technologies.vn>
2022-08-18 09:31:37 +0700

    main3 test

--- src/test2.java
+++ src/test2.java
@@ -1,5 +1,7 @@
 public class test2 {
     void toafn(){
+        System.out.println("88 ") ;
+        System.out.println("77 ") ;
         System.out.println("testing ") ;
         System.out.println("testing ") ;
         System.out.println("3322 ") ;
