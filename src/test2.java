a7ec901d2084487d1a01eb6100ef7042d72e4491
Phan Thanh Toan <toanpt@hybrid-technologies.vn>
2022-08-18 11:27:37 +0700

    main_s2 c

--- src/test2.java
+++ src/test2.java
@@ -2,12 +2,18 @@ public class test2 {
     void toafn(){
         System.out.println("8fff8 ") ;
         System.out.println("kkkkkk ") ;
-        System.out.println("5 ") ;
-        System.out.println("5 ") ;
-        System.out.println("5 ") ;
-        System.out.println("ewưe ") ;
-        System.out.println("ewew ") ;
-        System.out.println("5 ưeew") ;
+
+        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
+        System.out.println("kkkkkk ") ;
         System.out.println("ewew ") ;
         System.out.println("ytyty ") ;
     }
