b3580c7f60927e3d608ce5ed0e83ad7fbee415cb
Phan Thanh Toan <toanpt@hybrid-technologies.vn>
2022-08-18 11:20:12 +0700

    main test

--- src/test2.java
+++ src/test2.java
@@ -1,7 +1,7 @@
 public class test2 {
     void toafn(){
         System.out.println("8fff8 ") ;
-        System.out.println("7fff7 ") ;
+        System.out.println("kkkkkk ") ;
         System.out.println("5 ") ;
         System.out.println("5 ") ;
         System.out.println("5 ") ;
