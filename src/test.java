554fb3e4135e161b20a876df5ddcf351dc87c4e9
Phan Thanh Toan <toanpt@hybrid-technologies.vn>
2022-08-18 09:06:12 +0700

    main test

--- src/test.java
+++ src/test.java
@@ -1,15 +1,6 @@
 public class test {
     public static void main(String[] args) {
-int a;
-int b ;
-int c ;
 
-        System.out.println("testing");
-        System.out.println("testing");
-        System.out.println("testing");
-        System.out.println("testing222");
-        System.out.println("test222ing");
-        System.out.println("testing");
         System.out.println("testing");
         System.out.println("testing");
         System.out.println("testing");
