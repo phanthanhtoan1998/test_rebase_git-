e51bf9bf4989efde4f62e9be1e5446f7a5870dc6
Phan Thanh Toan <toanpt@hybrid-technologies.vn>
2022-08-18 13:24:31 +0700

    mainc2

--- src/test2.java
+++ src/test2.java
@@ -1,35 +1,7 @@
 public class test2 {
     void toafn(){
-        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;
 
-        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;            System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;    System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;        System.out.println("8fff8 ") ;
-        System.out.println("kkkkkk ") ;
-        System.out.println("ewew ") ;
+
         System.out.println("ytyty ") ;
         System.out.println("ytyty ") ;
         System.out.println("2222 ") ;
